73425422a3041d06812e088f67db2d367ab552d1
ADEYEMO <ADEYEMO>
2016-08-22 17:41:43 -1200

    Updated Post and Get files

--- search.js
+++ search.js
@@ -5,13 +5,16 @@ var rl = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('What One Programming language do you understand the most :', answer => {
+rl.question('What is The Programming language you understand the most :', answer => {
 
   var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+answer+'&limit=10&namespace=0&format=json';
 
      request.post(url, function (error, response, body) {
      if (!error && response.statusCode == 200) {
-         console.log(JSON.parse(response.body), url);
+       console.log();
+       console.log("Printing out Result" + "\n")
+       var newvar = JSON.parse(response.body);
+         console.log(newvar[1]);
      }
 
 })
